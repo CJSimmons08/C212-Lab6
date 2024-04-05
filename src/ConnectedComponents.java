997d101668f0a1392c26f83c24309cc44ea7b0f3
Connor Simmons <coshnor@gmail.com>
2024-04-05 11:22:33 -0400

    started on... something at least

--- src/ConnectedComponents.java
+++ src/ConnectedComponents.java
@@ -9,6 +9,20 @@ public class ConnectedComponents {
     public static <V> void
     connected_components(Graph<V> G, Map<V, V> representative) {
         // your code goes here
+        /*do search, create adjacency list, then whichever vertex is connected to the
+        * most vertices is the representative for those vertices, if a vertex isn't
+        * connected to any, set itself as its representative, maybe have temp list to
+        * be removing vertices from, that way if there are vertices left after
+        * choosing first representative, do the same but with whichever one now has
+        * the most connections*/
+        UndirectedAdjacencyList connections = depthSearch(G);
+
+    }
+
+    public static <V> UndirectedAdjacencyList depthSearch(Graph<V> G){
+        UndirectedAdjacencyList connections = new UndirectedAdjacencyList(G.numVertices());
+
+        return null;
     }
 
 
